fdb3627573eaccfdbd14fb932e7a9e6a7b8b5046
Marcin <marcin.juranek2@gmail.com>
2018-10-22 09:58:16 +0200

    First Commit, simulating Stock Exchange, three separate project: StockDateSource, StockMVC, StockUnitTest

---
+++ Test_StockExchange/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_StockExchange
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
